c817bce109d05b6b7006172489c2b3ea19b70e53
İsmail Karslı <ismail@karsli.net>
2024-02-12 14:29:51 +0300

    Add createdAt and updatedAt fields to User and Community models

--- server/api/community/index.get.ts
+++ server/api/community/index.get.ts
@@ -28,6 +28,9 @@ export default defineEventHandler(async function (event) {
       },
       skip,
       take,
+      orderBy: {
+        createdAt: "desc",
+      },
     }),
     prisma.community.count(),
   ]);
