236942e53b2619d6a9ddb130db3b2ab3de847a62
İsmail Karslı <ismail@karsli.net>
2024-06-20 15:21:05 +0300

    add communities more frequently

--- server/plugins/periodic-jobs.ts
+++ server/plugins/periodic-jobs.ts
@@ -5,8 +5,9 @@ import ms from "ms";
 const prisma = new PrismaClient();
 
 export default defineNitroPlugin(async () => {
-  loop(addCommunities, ms("1 minute"));
-  loop(updateFollows, ms("1 hour"));
+  loop(updateFollows, ms("1 day"));
+  loop(addCommunities, ms("1 minute")); // fetch newest communities directly from instances
+  loop(addCommunitiesFromLemmyverse, ms("1 day"));
 });
 
 const loop = async (callback: () => void, timeout = 0) => {
@@ -76,6 +77,62 @@ async function addCommunities() {
     where: {
       enabled: true,
       auto_add: true,
+      bot_name: { not: null },
+      bot_pass: { not: null },
+    },
+  });
+
+  for (const instance of instances) {
+    try {
+      const client = await getHttpClient(instance.host, {
+        username_or_email: instance.bot_name!,
+        password: instance.bot_pass!,
+      });
+
+      const { communities } = await client.listCommunities({
+        type_: "Local",
+        sort: "New",
+        page: 1,
+        limit: 50,
+      });
+
+      for (const c of communities) {
+        const exists = await prisma.community.count({
+          where: {
+            name: c.community.name.toLowerCase(),
+            instanceId: instance.id,
+          },
+        });
+        if (!exists) {
+          const addedCommunity = await prisma.community.create({
+            data: {
+              name: c.community.name.toLowerCase(),
+              instanceId: instance.id,
+            },
+            include: {
+              instance: true,
+            },
+          });
+          conditionalFollowWithAllInstances(addedCommunity);
+        }
+      }
+    } catch (error) {
+      console.error(
+        `Error while adding communities from ${instance.host}`,
+        error
+      );
+    }
+  }
+
+  console.info("Finished adding communities");
+}
+
+async function addCommunitiesFromLemmyverse() {
+  console.info("Adding communities from Lemmyverse");
+  const instances = await prisma.instance.findMany({
+    where: {
+      enabled: true,
+      auto_add: true,
     },
   });
   const instanceIdMap = Object.fromEntries(
@@ -123,5 +180,5 @@ async function addCommunities() {
     );
   }
 
-  console.info("Finished adding communities automatically");
+  console.info("Finished adding communities from Lemmyverse");
 }
