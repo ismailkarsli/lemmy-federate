d4ab35549f4d13958070e18101cc8c32c879440f
İsmail Karslı <ismail@karsli.net>
2024-09-15 18:30:58 +0300

    don't render tooltips on server (to fix hydration errors)

--- components/InfoTooltip.vue
+++ components/InfoTooltip.vue
@@ -2,22 +2,24 @@
 import { ref } from "vue";
 
 const props = defineProps<{
-	text: string;
+  text: string;
 }>();
 
 const show = ref(false);
 </script>
 
 <template>
-  <v-tooltip :text="props.text" v-model="show">
-    <template v-slot:activator="{ props }">
-      <v-btn
-        v-bind="props"
-        icon="mdi-information"
-        variant="text"
-        density="compact"
-        @click="show = !show"
-      />
-    </template>
-  </v-tooltip>
+  <ClientOnly>
+    <v-tooltip :text="props.text" v-model="show">
+      <template v-slot:activator="{ props }">
+        <v-btn
+          v-bind="props"
+          icon="mdi-information"
+          variant="text"
+          density="compact"
+          @click="show = !show"
+        />
+      </template>
+    </v-tooltip>
+  </ClientOnly>
 </template>
