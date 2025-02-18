defe6175ee883bbeeb94ac9682556c42f6cba9f4
Dominik Chrastecky <dominik@chrastecky.cz>
2025-02-18 23:20:10 +0100

    Add another known software

--- src/lib/messaging.ts
+++ src/lib/messaging.ts
@@ -14,6 +14,7 @@ export function createMessage(software: string, from: string | Actor<string>, to
     switch (software.toUpperCase()) {
         case "LEMMY":
         case "MBIN":
+        case "DCH_BLOG":
             return [lemmyStyleMessage];
         case "MASTODON":
             return [mastodonStyleMessage];
