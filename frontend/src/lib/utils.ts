0fcad94239413a17c9c0487d8602902e6ee68a44
İsmail Karslı <ismail@karsli.net>
2025-08-28 12:42:56 +0300

    Add special case for WordPress name formatting

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,9 +1,14 @@
 export function getHumanReadableSoftwareName(name: string): string {
+	// No name
 	if (!name) return "";
+
+	// Special cases
+	if (name === "wordpress") return "WordPress";
+
 	// Split the string by underscores or one or more whitespace characters
 	const words = name.split(/[\s_]+/);
 
-	// Capitalize the first letter of each word and make the rest of the word lowercase
+	// Capitalize the first letter of each word and keep the rest as is
 	const capitalizedWords = words.map(
 		(word) => word.charAt(0).toUpperCase() + word.slice(1),
 	);
